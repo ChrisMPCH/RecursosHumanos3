8d54f13c8ad551df0fb9593e6f00217643d34765
ChrisMPCH <137126465+ChrisMPCH@users.noreply.github.com>
2025-04-14 14:35:26 -0600

    Update Program.cs

--- ControlEscolarP2/Program.cs
+++ ControlEscolarP2/Program.cs
@@ -14,22 +14,20 @@ namespace RecursosHumanos
         static void Main()
         {
             //Activa los estilos visuales modernos de Windows
-            // Sin esta línea, los controles como botones,
-            // cuadros de texto y pestañas se verán con el estilo antiguo de Windows 95/98.
+            // Sin esta lï¿½nea, los controles como botones,
+            // cuadros de texto y pestaï¿½as se verï¿½n con el estilo antiguo de Windows 95/98.
             Application.EnableVisualStyles();
             //Se usa para evitar que los controles usen el motor de renderizado de texto
             //antiguo de .NET 1.0, que era menos eficiente.
             Application.SetCompatibleTextRenderingDefault(false);
 
-            //Inicia la aplicación con la ventana de inicio
+            //Inicia la aplicaciï¿½n con la ventana de inicio
 
             frmLogin login_form = new frmLogin(); 
             if (login_form.ShowDialog() == DialogResult.OK) //El show dialog espera una respuesta
             {
                 Application.Run(new MDIRecursosHumanos());
             }
-
-
         }
     }
 }
