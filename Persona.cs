b2a204a44385f2617c32c658d0485fb1ecc87215
EmmanuelPad <L22280396@toluca.tecnm.mx>
2025-04-17 22:37:31 -0600

    Add files via upload

---
+++ Persona.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RecursosHumanos.Model
+{
+    public class Persona
+    {
+        private string v;
+
+        //propiedades de la persona
+        
+        /// <summary>
+        /// ID de la Persona
+        /// </summary>
+        public int IdPersona { get; set; }
+
+        /// <summary>
+        /// Nombre de la persona
+        /// </summary>
+        public string Nombre { get; set; }
+       
+        /// <summary>
+        /// Apellido paternp
+        /// </summary>
+        public string ApellidoPaterno { get; set; }
+        
+        /// <summary>
+        /// Apellido materno
+        /// </summary>
+        public string ApellidoMaterno { get; set; }
+
+        /// <summary>
+        /// Correo electrónico de la persona
+        /// </summary>
+        public string Correo { get; set; }
+        
+        /// <summary>
+        /// Número de teléfono de la persona
+        /// </summary>
+        public string Telefono { get; set; }
+        
+        /// <summary>
+        /// CURP (Clave Única de Registro de Población) de la persona
+        /// </summary>
+        public string Curp { get; set; }
+
+        /// <summary>
+        /// Fecha de nacimiento de la persona
+        /// </summary>
+        public DateTime? FechaNacimiento { get; set; }//no es obligatoria puede ser nulo
+
+        /// <summary>
+        /// Indica si la persona está activa en el sistema
+        /// </summary>
+        public bool Estatus { get; set; }
+        
+        /// <summary>
+        /// RFC de la persona
+        /// </summary>
+        public string RFC { get;set; }
+        
+        /// <summary>
+        /// Genero de la persona 'M':Mujer 'H':Hombre 
+        /// </summary>
+        public string Genero { get;set; }
+
+        /// <summary>
+        /// Direccion de la perosona
+        /// </summary>
+        public string Direccion { get;set; }
+
+        //para recibir objeto con datos vacios, cuando se quiere depositar datos en el objeto
+        public Persona()
+        {
+            Nombre = string.Empty;
+            ApellidoPaterno = string.Empty;
+            ApellidoMaterno = string.Empty;
+            Correo = string.Empty;
+            Telefono = string.Empty;
+            Curp = string.Empty;
+            Estatus = true;
+            RFC= string.Empty;
+            Genero= string.Empty;
+            Direccion= string.Empty;
+        }
+        
+        
+    }
+}
---
+++ Usuario.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursosHumanos.Model
+{
+    internal class Usuario
+    {
+        /// <summary>
+        /// ID generado para el usuario
+        /// </summary>
+        public int IdUsuario {  get; set; }
+        /// <summary>
+        /// Id de la persona a que le van a hacer el ususario
+        /// </summary>
+        public int IdPersona { get; set; }
+        /// <summary>
+        /// Id del rol a asignar
+        /// </summary>
+        public int IdRol {  get; set; }
+        /// <summary>
+        /// Nombre que le pondran al nuevo usuario 
+        /// </summary>
+        public string NombreUsuario { get; set; }
+        /// <summary>
+        /// Contraseña de Acceso
+        /// </summary>
+        public string Contraseña { get; set;} 
+        /// <summary>
+        /// Fecha de cracion del Usuario
+        /// </summary>
+        public DateTime? CreacionUsuario {  get; set; }
+        /// <summary>
+        /// Ultima fecha de inicio de secion 
+        /// </summary>
+        public DateTime? Fechainiciodesesion { get;set; }
+        /// <summary>
+        /// Estatus del Usuario "True":Activo "False":Inactivo
+        /// </summary>
+        public bool EstatusUsuario { get; set;}
+        /// <summary>
+        /// Todos los datos de la persona a que se le va a hacer el usuario
+        /// </summary>
+        public Persona DatosPersona { get; set; }
+        /// <summary>
+        /// Constructor del usuario vacio
+        /// </summary>
+        public Usuario ()
+        {
+            NombreUsuario = string.Empty;
+            Contraseña = string.Empty;
+            CreacionUsuario = DateTime.Now;
+            Fechainiciodesesion = DateTime.Now;
+            EstatusUsuario = true;
+            DatosPersona = new Persona ();
+        }
+
+
+
+    }
+}
