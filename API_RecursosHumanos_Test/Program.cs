26a6f54e2c9f2d0cc7cee2e0d5080cd2ddcfe93b
ChrisMPCH <137126465+ChrisMPCH@users.noreply.github.com>
2025-05-29 21:28:43 -0600

    intento 5

--- API_RecursosHumanos_Test/Program.cs
+++ API_RecursosHumanos_Test/Program.cs
@@ -1,15 +1,27 @@
+using RecursosHumanosCore.Controller;
+using RecursosHumanosCore.Controllers;
+using RecursosHumanosCore.Data;
+using RecursosHumanosCore.DataAccess;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configura aquí tus cadenas de conexión si es necesario
+// Por ejemplo:
+// PosgresSQLAccess.ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Registra aquí todos los controladores y servicios que inyectas en RecursosHumanosControllerAPI_test
+builder.Services.AddScoped<ContratoController>();
+builder.Services.AddScoped<UsuariosController>();
+builder.Services.AddScoped<EmpleadosDataAccess>();
+builder.Services.AddScoped<AsistenciaDataAccess>();
+builder.Services.AddScoped<AusenciaController>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
